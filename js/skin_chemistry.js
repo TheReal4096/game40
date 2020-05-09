97df43a428f05caa7f4089370c69aac8595d09f3
TheReal2048 <64551777+TheReal2048@users.noreply.github.com>
2020-05-09 08:54:55 -0700

    Add chemistry skin.

--- js/skin_chemistry.js
+++ js/skin_chemistry.js
@@ -10,40 +10,40 @@ window.requestAnimationFrame(function () {
     this.applyClasses(wrapper, classes);
     inner.classList.add("tile-inner");
     switch (tile.value) {
-    case 2:
+    case 1:
       inner.textContent = "H";
       break;
-    case 4:
+    case 2:
       inner.textContent = "He";
       break;
-    case 8:
+    case 4:
       inner.textContent = "Li";
       break;
-    case 16:
+    case 8:
       inner.textContent = "Be";
       break;
-    case 32:
+    case 16:
       inner.textContent = "B";
       break;
-    case 64:
+    case 32:
       inner.textContent = "C";
       break;
-    case 128:
+    case 64:
       inner.textContent = "N";
       break;
-    case 256:
+    case 128:
       inner.textContent = "O";
       break;
-    case 512:
+    case 256:
       inner.textContent = "F";
       break;
-    case 1024:
+    case 512:
       inner.textContent = "Ne";
       break;
-    case 2048:
+    case 1024:
       inner.textContent = "Na";
       break;
-    case 4096:
+    case 2048:
       inner.textContent = "Mg";
       break;
     }
